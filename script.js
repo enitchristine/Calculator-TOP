84a8d6774786092ca73bad4d07808c69ae77bcae
Christine <enit.christine@gmail.com>
2025-04-22 19:58:57 +1000

    Added catch for invalid input

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function divide(a,b){
 }
 
 function operate(num1,num2, operator){
-    num1=parseInt(num1);
-    num2=parseInt(num2);
+    num1=parseFloat(num1);
+    num2=parseFloat(num2);
 
     switch (operator){
         case "+":
@@ -38,44 +38,64 @@ function makeEvent(id){
     console.log(button.innerHTML);
     button.addEventListener("click",(e)=>{
         console.log("clicked");
-        resultCheck=""; //checks if = was pressed recently
+        if (resultCheck){ //checks if = was pressed just before, clear
+            display="";
+            resultCheck=false;
+        }; 
         display+=button.innerHTML;
         inputScreen.textContent=display;
     });
 }
 
-
 let button;
-let operator=["+","-","x",":","C"];
-
+let others=["+","-","x",":","."];
+let operators=["+","-","x",":"];
+let resultCheck = false;
 const inputScreen = document.querySelector("#input");
 let display="";
-
+const resultScreen = document.querySelector("#result");
 
 for (let i=0; i<10;i++){ //add event listener for number buttons
     makeEvent(i);
 }
 
-for (op in operator){ //add event listener for operator buttons
-    makeEvent(operator[op]);
+for (icon in others){ //add event listener for other buttons
+    makeEvent(others[icon]);
 }
 
 const equal=document.getElementById("=");
-equal.addEventListener("click",(e)=>{
+equal.addEventListener("click",(e)=>{   //operate on the numbers on screen when "=" is clicked
 
-    //operate on the numbers on screen
-
-    let numbers=display.split(/[x:+-]/);
-    console.log(numbers);
-    let op=display.split(/[1234567890]/);
+    let numbers=display.split(/[x:+-]/); //get 
+    numbers=numbers.filter((x)=>x!="");
+    let op=display.split(/[1234567890.]/).join("").split("");
     op=op.filter((x)=>x!="");
-    console.log(op);
-    let i=0;
-    console.log(numbers.reduce((a,b)=>{
-        let result=operate(a,b,op[i]);
-        i++;
-        return result;
-    }
-    ));
 
-})
+    if (numbers.length<=op.length){
+        resultScreen.textContent="ERROR";
+    }else{
+        let i=0;
+        let results=numbers
+            .reduce((a,b)=>{ 
+                let result=operate(a,b,op[i]);
+                i++;
+                return result;
+            }
+        );
+
+        if (results%1>0){   // if results has decimals
+            results=results.toFixed(2);
+        }
+
+        resultScreen.textContent=results;
+        
+    }
+    resultCheck=true;
+}
+)
+
+const clear=document.getElementById("C");
+clear.addEventListener("click",(e)=>{
+    display="";
+    inputScreen.textContent=display;
+});
