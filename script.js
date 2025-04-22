464760cc1e4b7a9ff4bbf91c36896586e9a0a8b9
Christine <enit.christine@gmail.com>
2025-04-22 12:01:28 +1000

    Added operator() function

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ function divide(a,b){
     return a/b;
 }
 
+function operate(num1,num2, operator){
+
+    switch (operator){
+        case "+":
+            return add(num1,num2);
+        case "-":
+            return subtract(num1,num2);
+        case "x":
+            return multiply(num1,num2);
+        case ":":
+            return divide(num1,num2);
+    }
+
+}
