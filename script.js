45049d127f45b8e759d0c02a1f2354708a6116c5
Christine <enit.christine@gmail.com>
2025-04-22 19:17:09 +1000

    Calculator operates when '=' is clicked

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ function divide(a,b){
 }
 
 function operate(num1,num2, operator){
+    num1=parseInt(num1);
+    num2=parseInt(num2);
 
     switch (operator){
         case "+":
@@ -30,3 +32,50 @@ function operate(num1,num2, operator){
     }
 
 }
+
+function makeEvent(id){
+    const button=document.getElementById(id);
+    console.log(button.innerHTML);
+    button.addEventListener("click",(e)=>{
+        console.log("clicked");
+        resultCheck=""; //checks if = was pressed recently
+        display+=button.innerHTML;
+        inputScreen.textContent=display;
+    });
+}
+
+
+let button;
+let operator=["+","-","x",":","C"];
+
+const inputScreen = document.querySelector("#input");
+let display="";
+
+
+for (let i=0; i<10;i++){ //add event listener for number buttons
+    makeEvent(i);
+}
+
+for (op in operator){ //add event listener for operator buttons
+    makeEvent(operator[op]);
+}
+
+const equal=document.getElementById("=");
+equal.addEventListener("click",(e)=>{
+
+    //operate on the numbers on screen
+
+    let numbers=display.split(/[x:+-]/);
+    console.log(numbers);
+    let op=display.split(/[1234567890]/);
+    op=op.filter((x)=>x!="");
+    console.log(op);
+    let i=0;
+    console.log(numbers.reduce((a,b)=>{
+        let result=operate(a,b,op[i]);
+        i++;
+        return result;
+    }
+    ));
+
+})
